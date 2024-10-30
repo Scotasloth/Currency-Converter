07e3c1ca8d525285f3f244b22b8a31f5a5c941dc
Scotasloth <rossmcc76@gmail.com>
2024-10-30 20:44:54 +0000

    Implemented user input

--- convert.cs
+++ convert.cs
@@ -3,10 +3,19 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
-class program
+class Program
 {
 
+    static async Task main(string[] args)
+    {
 
+        Console.WriteLine("How much to convert?");
+        decimal amount = Convert.ToDecimal(Console.ReadLine());
 
-    
+        Console.WriteLine("What is your currency?");
+        string homeCurr = Console.ReadLine().ToUpper();
+
+        Console.WriteLine("What do you want to convert to?");
+        string newCurr = Console.ReadLine().ToUpper()
+    }
 }
